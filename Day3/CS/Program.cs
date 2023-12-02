bdab7d6d7f7a33991515135483e0f82b4fb86f44
Vincent van Setten <vvampus@gmail.com>
2023-12-02 13:29:41 +0100

    Prepare Day 3

---
+++ Day3/CS/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AOC
+{
+    public static class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            // Read Input Data
+            var inputData = await InputManager.ReadLinesByFilename(args);
+
+            // Part 1
+            // Console.WriteLine($"Puzzle Part 1 Result: {await Part1.}");
+
+            // Part 2
+            // Console.WriteLine($"Puzzle Part 2 Result: {await Part2");
+
+        }
+    };
+}
---
+++ Day3/CS/PuzzleAnswers.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AOC
+{
+
+    public static class Part1
+    {
+        // TODO
+    }
+
+    public static class Part2
+    {
+        // TODO
+    }
+}
