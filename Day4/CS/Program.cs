c34e20d51ae403e47efc7ade04464322e557fe6e
Vincent van Setten <vvampus@gmail.com>
2023-12-03 23:29:22 +0100

    Day 4 preparations

---
+++ Day4/CS/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AOC
+{
+    public static class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            // Read Input Data
+            var inputData = await InputManager.ReadLinesByFilename(args);
+
+            // Part 1
+            // Console.WriteLine($"Puzzle Part 1 Result: {Part1.}");
+
+            // Part 2
+            // Console.WriteLine($"Puzzle Part 2 Result: {Part2.}");
+
+        }
+    };
+}
